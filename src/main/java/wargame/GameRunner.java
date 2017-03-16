88abdbeac296e6de2ede772cb444c90a633c1dca
Jen <jhammond.software.engineer@gmail.com>
2017-03-15 18:23:46 -0700

    basic gradle setup and main class

---
+++ src/main/java/wargame/GameRunner.java
@@ -0,0 +1,11 @@
+package wargame;
+
+/**
+ * Created by Jen on 3/15/2017.
+ */
+public class GameRunner {
+
+    public static void main(String[]args){
+        System.out.println("This is war!");
+    }
+}
